343573ab585434d212af2bae955134e7e2e89bf0
it-AVNG <it.avng@gmail.com>
2023-09-22 07:07:52 +0700

    add code for creating an subset

--- code/mindgap.py
+++ code/mindgap.py
@@ -1,4 +1,16 @@
 import pandas as pd
 
+#read the file
 df = pd.read_csv('../data/gapminder.tsv', sep='\t')
-print(df.dtypes)
+columns_list = list(df.columns)
+#iterate through the columns
+my_list = [item for item in columns_list]
+print(my_list)
+print(df.columns[0])
+#creating subset
+string1,string2,string3, string4 = 'country', 'continent','year','lifeExp'
+listing = [string1,string2,string3, string4]
+subset = df[listing]
+subset2 = df [['country', 'continent','year','gdpPercap']]
+print(subset)
+print(subset2)
